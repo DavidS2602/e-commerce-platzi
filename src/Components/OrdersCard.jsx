2ba6aaf5b50ceb36f83f0c7c9b7558593aa8dc0c
David Gaspar <davsangas@hotmail.com>
2023-07-04 11:08:33 -0600

    Styles to OrdersCard

--- src/Components/OrdersCard.jsx
+++ src/Components/OrdersCard.jsx
@@ -1,16 +1,29 @@
-const OrdersCard = props => {
-    const { totalPrice, totalProducts } = props
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BsCalendarDate } from "react-icons/bs";
+
+const OrdersCard = (props) => {
+    const { totalPrice, totalProducts } = props;
     return (
-        <div className="flex justify-between items-center my-2 border border-black">
-            <p>
-                <span>01.02.23</span>
-                <span>{totalProducts}</span>
-                <span>{totalPrice}</span>
-
+        <div className="flex justify-between items-center my-2 border border-black w-44 px-2 rounded-lg">
+            <p className="flex flex-col">
+                <div className="flex items-center gap-2">
+                    <span>
+                        <BsCalendarDate />
+                    </span>
+                    <span>01.02.23</span>
+                </div>
+                <div className="flex items-center gap-2">
+                    <span>
+                        <AiOutlineShoppingCart />
+                    </span>
+                <span>{totalProducts} articles</span>
+                </div>
             </p>
+                <div>
+                    <span className="text-xl font-semibold text-green-500">${totalPrice}</span>
+                </div>
         </div>
-
     );
-}
+};
 
 export default OrdersCard;
